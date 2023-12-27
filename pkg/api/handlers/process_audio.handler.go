3424e4e06fb66e08529603d60dd81919888b91cc
Biel Carpi <biel.carpi@outlook.com>
2023-12-27 20:30:31 +0100

    feat(whisper): whisper support working

--- pkg/api/handlers/process_audio.handler.go
+++ pkg/api/handlers/process_audio.handler.go
@@ -2,14 +2,55 @@ package handlers
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"io"
+	"net/http"
 	"nia-backend/pkg/openai"
+	"os"
+	"path"
 )
 
 func ProcessAudioHandler(c *gin.Context) {
-	client := openai.OpenAIClientProvider()
+	client := openai.ClientProvider()
 	// TODO handle 429: exceeded current plan limits
 
-	text, err := client.SpeechToText(c, c.Request.Body)
+	uploadDir := "./uploads"
+
+	// Parse the multipart form
+	err := c.Request.ParseMultipartForm(10 << 20) // Max 10 MB file size
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "File too large or invalid form"})
+		return
+	}
+
+	// Retrieve the file from the form data
+	file, _, err := c.Request.FormFile("file") // "file" is the name attribute in the form
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid file"})
+		return
+	}
+	defer file.Close()
+
+	// Generate a unique filename, for example using a UUID
+	newFileName := uuid.New().String() + ".mp3"
+
+	// Create the file in the designated directory
+	filePath := path.Join(uploadDir, newFileName)
+	out, err := os.Create(filePath)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Unable to create the file"})
+		return
+	}
+	defer out.Close()
+
+	// Copy the file content to the new file
+	_, err = io.Copy(out, file)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to save the file"})
+		return
+	}
+
+	text, err := client.SpeechToText(c, filePath)
 	if err != nil {
 		c.JSON(500, gin.H{
 			"error": err.Error(),
--- pkg/openai/openai.api.go
+++ pkg/openai/openai.api.go
@@ -3,7 +3,6 @@ package openai
 import (
 	"context"
 	"github.com/sashabaranov/go-openai"
-	"io"
 	"nia-backend/config"
 )
 
@@ -11,15 +10,15 @@ type Client struct {
 	client *openai.Client
 }
 
-func OpenAIClientProvider() *Client {
+func ClientProvider() *Client {
 	client := openai.NewClient(config.GetConfig().OpenAIAPIKey)
 	return &Client{client: client}
 }
 
-func (ai *Client) SpeechToText(ctx context.Context, audio io.Reader) (string, error) {
+func (ai *Client) SpeechToText(ctx context.Context, fileName string) (string, error) {
 	req := openai.AudioRequest{
-		Model:  openai.Whisper1,
-		Reader: audio,
+		Model:    openai.Whisper1,
+		FilePath: fileName,
 	}
 	resp, err := ai.client.CreateTranscription(ctx, req)
 	if err != nil {
@@ -31,10 +30,12 @@ func (ai *Client) SpeechToText(ctx context.Context, audio io.Reader) (string, er
 
 func (ai *Client) SendToGPT3(ctx context.Context, prompt string) (string, error) {
 	req := openai.CompletionRequest{
-		Model:     openai.GPT3Dot5Turbo1106,
+		Model:     openai.GPT4,
 		MaxTokens: 500,
 		Prompt:    prompt,
 	}
+
+	print(prompt)
 
 	resp, err := ai.client.CreateCompletion(ctx, req)
 	if err != nil {
