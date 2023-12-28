0d02839636dbd70211eb5241cc39652b9f1eb12e
Biel Carpi <biel.carpi@outlook.com>
2023-12-28 16:17:24 +0100

    feat(whisper): improve file reception

--- pkg/api/handlers/process_audio.handler.go
+++ pkg/api/handlers/process_audio.handler.go
@@ -6,81 +6,66 @@ import (
 	"io"
 	"net/http"
 	"nia-backend/pkg/openai"
+	"nia-backend/pkg/util"
 	"os"
-	"path"
+	"path/filepath"
 )
+
+const UPLOAD_DIR = "./uploads"
 
 func ProcessAudioHandler(c *gin.Context) {
 	client := openai.ClientProvider()
 	// TODO handle 429: exceeded current plan limits
 
-	uploadDir := "./uploads"
+	// Receive the audio from the client and temporarily store it
+	filePath, err := receiveSpeech(c)
+	defer os.Remove(filePath)
 
-	// Parse the multipart form
-	err := c.Request.ParseMultipartForm(10 << 20) // Max 10 MB file size
+	// Convert the audio to text
+	text, err := client.SpeechToText(c, filePath)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "File too large or invalid form"})
+		util.SendError(c, err)
 		return
 	}
 
-	// Retrieve the file from the form data
-	file, _, err := c.Request.FormFile("file") // "file" is the name attribute in the form
+	// Send the text to GPT-3
+	generatedText, err := client.SendToGPT3(c, text)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid file"})
+		util.SendError(c, err)
 		return
+	}
+
+	// Convert the text to speech
+	speech, err := client.TextToSpeech(c, generatedText)
+	if err != nil {
+		util.SendError(c, err)
+		return
+	}
+
+	// Stream the speech back to the client
+	err = sendSpeech(c, speech)
+	if err != nil {
+		util.SendError(c, err)
+		return
+	}
+}
+
+func receiveSpeech(c *gin.Context) (string, error) {
+	fileName := uuid.New().String() + ".ogg"
+	filePath := filepath.Join(UPLOAD_DIR, fileName)
+	file, err := os.Create(filePath)
+	if err != nil {
+		return "", err
 	}
 	defer file.Close()
 
-	// Generate a unique filename, for example using a UUID
-	newFileName := uuid.New().String() + ".mp3"
-
-	// Create the file in the designated directory
-	filePath := path.Join(uploadDir, newFileName)
-	out, err := os.Create(filePath)
+	// Copy the data from the request body to the file
+	_, err = io.Copy(file, c.Request.Body)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Unable to create the file"})
-		return
-	}
-	defer out.Close()
-
-	// Copy the file content to the new file
-	_, err = io.Copy(out, file)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to save the file"})
-		return
+		return "", err
 	}
 
-	text, err := client.SpeechToText(c, filePath)
-	if err != nil {
-		c.JSON(500, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
-
-	generatedText, err := client.SendToGPT3(c, text)
-	if err != nil {
-		c.JSON(500, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
-
-	speech, err := client.TextToSpeech(c, generatedText)
-	if err != nil {
-		c.JSON(500, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
-
-	err = sendSpeech(c, speech)
-	if err != nil {
-		c.JSON(500, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
+	return filePath, nil
 }
 
 func sendSpeech(c *gin.Context, speech io.ReadCloser) error {
--- pkg/util/helpers.go
+++ pkg/util/helpers.go
@@ -1,1 +1,12 @@
 package util
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func SendError(c *gin.Context, err error) {
+	c.JSON(http.StatusInternalServerError, gin.H{
+		"error": err.Error(),
+	})
+}
